37cd767b53c615870815d8a1891a05b32e4ac9a8
SATYA-GARLAPATI09 <satyagarlapati8@gmail.com>
2025-09-06 12:36:08 +0530

    satya

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("hi mama this is script file   ");
+let name = "satya";
+console.log(name);
+
+let age = null;
+console.log(age);
+
+let isLoggedIn = false;
+console.log(isLoggedIn);
+
+let person={
+    name:"satya",
+    age:21,
+}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+
+console.log(typeof (person) );
+console.log(typeof person.name );
+console.log(typeof person.age );
+
+
+console.log(typeof (age) );
+console.log(typeof (name));
+console.log(typeof (isLoggedIn) );
+console.log(typeof (null) );
+
